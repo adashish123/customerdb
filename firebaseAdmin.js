9f0990a3847fa2b8aa2e32e0643cf5800e2c48f4
qrapidnow <qrapidnow@gmail.com>
2024-07-28 17:16:10 +0530

    updated order.controller

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -26,7 +26,7 @@ const firestore = new Firestore({
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
   },
-  grpc
+  sslCreds: grpc.credentials.createSsl()
 });
 
 module.exports = firestore;
