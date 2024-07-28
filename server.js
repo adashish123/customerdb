1f1d4b1214f718439573ae35105fb3d0e8c089df
qrapidnow <qrapidnow@gmail.com>
2024-07-28 16:53:48 +0530

    updated order.controller

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', PUT, 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   preflightContinue: false,
